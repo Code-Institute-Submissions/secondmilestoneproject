f823900da935b0f7ee98e7908abf721395acbe91
codingbas <basvanhoolwerff1980@gmail.com>
2019-04-19 12:18:39 +0000

    add Google Maps places and solved typo issue

--- README.md
+++ README.md
@@ -1,18 +1,11 @@
+Milestone 2 - Search for coffee bars in Amsterdam - Interactive Frontend Development Milestone Project
+This website is available to view here - 
 
-     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.
-    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'
-    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, 
-    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.
-     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
-    ----------------------------------------------------------------- 
+This website has been created to allow users to search for a coffee bar or place in Amsterdam and to find out more information on the accommodation, food and drink, and points of interest available within the area that they have searched for. 
+With the help of Google Maps JavaScript API, users can obtain this information through pop up windows that are triggered when a radio button has been checked, e.g. when the accommodation radio button is checked, markers will appear on the map displaying accommodation within the area that has been searched for. 
+The Maps API also allows users to view StreetView images and satellite imagery.
 
+UX
+This website is targeted towards users aiming to travel to a planned destination or towards users who want to find out more about accommodation, places to eat or attractions within an area they are curious about. 
+This type of user will want to know where places to stay and places to eat are, and also points of interest which they may like to visit on their holiday. My project is a suitable way of achieving this because it provides filters for the user to use when they search for a place to visit and also provides them with information such as addresses and telephone numbers of establishments they are interested in. It is an easy-to-use website - all the user has to do is type in their chosen location into the search bar, select a query and a filter, and then select a marker on the map to reveal the information they are seeking. Once they have found what they are looking for, the user can click on the place name in the info window which takes them to Google Maps, providing more information such as reviews and ratings. The user may also use the Streetview feature and satellite imagery to get a better understanding of what the location they searched for looks like.
 
-Hi there! Welcome to Cloud9 IDE!
-
-To get you started, create some files, play with the terminal,
-or visit http://docs.c9.io for our documentation.
-If you want, you can also go watch some training videos at
-http://www.youtube.com/user/c9ide.
-
-Happy coding!
-The Cloud9 IDE team
--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,29 +1,154 @@
+// Map variables
+var autocomplete;
+var map;
+var markers = [];
+var infoWindow;
+var places;
+
+// Initialise map
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: {
-            lat: 46.619261,
-            lng: -33.134766
-    }
-});
-        var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
-        var locations = [{
-            lat: 40.785091,
-            lng: -73.968285
-    }, {
-            lat: 41.084045,
-            lng: -73.874256
-    }, {
-            lat: 40.754932,
-            lng: -73.984016
-}];
-        var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-});
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: {lat: 54.5260, lng: 15.2551},
+        // Map controls allowing user to zoom in/out, switch to satellite mode and view streetview images
+        mapTypeControl: true,
+        zoomControl: true,
+        streetViewControl: true
     });
-        var markerCluster = new MarkerClusterer(map, markers, 
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+
+    // Info window with place details
+    infoWindow = new google.maps.InfoWindow({
+        content: document.getElementById('place-details')
+    });
+
+    // Autocomplete
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('autocomplete'));
+        places = new google.maps.places.PlacesService(map);
+        autocomplete.addListener('place_changed', onPlaceChanged); // Resets map to new area that has been searched
+        document.getElementById('accommodation-filter').addEventListener('change', onPlaceChanged);
+        document.getElementById('food-filter').addEventListener('change', onPlaceChanged);
 }
 
+// Select autocomplete query - return details of place and zoom into area
+function onPlaceChanged() {
+    if ($("#accommodation-filter").is(':checked')) {        
+        var place = autocomplete.getPlace();                //getPlace() retrieves place name for autocomplete query
+        if (place.geometry) {
+            map.panTo(place.geometry.location);             //Retrieves lng/lat of place
+            map.setZoom(15);
+            searchAccommodation();
+        }
+        else {
+            $('#autocomplete').attr("placeholder", "Search");
+        }
+    }
+    else if ($("#food-filter").is(':checked')) {
+        var place = autocomplete.getPlace();
+        if (place.geometry) {
+            map.panTo(place.geometry.location);
+            map.setZoom(15);
+            searchFoodAndDrink();
+        }
+        else {
+            $('#autocomplete').attr("placeholder", "Search");
+        }
+    }
+    
+        else {
+            $('#autocomplete').attr("placeholder", "Search");
+        }
+    }
+
+
+// Search for accommodation
+function searchAccommodation() {
+    var search = {
+        bounds: map.getBounds(),                //getBounds() retrieves lng/lat for corners of visible area of map
+        types: ['lodging', 'campground']
+    };
+    places.nearbySearch(search, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {         //If Places Service is working, create marker
+            clearResult();
+            clearMarker();
+            // Create marker
+            for (var x = 0; x < results.length; x++) {
+                markers[x] = new google.maps.Marker({
+                    position: results[x].geometry.location                  //Positions marker to searched location
+                });
+                // When user clicks accommodation marker, show details
+                markers[x].placeResult = results[x];
+                google.maps.event.addListener(markers[x], 'click', showInfoWindow);
+                setTimeout(dropMarkers(x), x * 100);
+            }
+        }
+    });
+}
+
+// Search for food and drink
+function searchFoodAndDrink() {
+    var search = {
+        bounds: map.getBounds(),
+        types: ['restaurant', 'bar']
+    };
+    places.nearbySearch(search, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            clearResult();
+            clearMarker();
+            // Create marker
+            for (var x = 0; x < results.length; x++) {
+                markers[x] = new google.maps.Marker({
+                    position: results[x].geometry.location
+                });
+                // When user clicks food and drink marker, show details
+                markers[x].placeResult = results[x];
+                google.maps.event.addListener(markers[x], 'click', showInfoWindow);
+                setTimeout(dropMarkers(x), x * 100);
+            }
+        }
+    });
+}
+
+// Clear markers when new radio option selected
+function clearMarker() {
+    for (var x = 0; x < markers.length; x++) {
+        if (markers[x]) {
+            markers[x].setMap(null);
+        }
+    }
+    markers = [];
+}
+
+// Add found results to map
+function dropMarkers(x) {
+    return function() {
+        markers[x].setMap(map);
+    };
+}
+
+function clearResult() {
+    var results = document.getElementById('results');
+    while (results.childNodes[0]) {
+        results.removeChild(results.childNodes[0]);
+    }
+}
+
+// Fetch place details
+function showInfoWindow() {
+    var marker = this;
+    places.getDetails({placeId: marker.placeResult.place_id},
+        function(place, status) {
+            if (status !== google.maps.places.PlacesServiceStatus.OK) {
+                return;
+            }
+            infoWindow.open(map, marker);
+            setPlaceDetails(place);
+        });
+}
+
+// Show place details in info window on map
+function setPlaceDetails(place) {
+    document.getElementById('url').innerHTML = '<a href="' + place.url + 'target="_blank">' + place.name + '</a>';
+    document.getElementById('address').textContent = place.vicinity;
+    document.getElementById('phone').textContent = place.formatted_phone_number;
+}
